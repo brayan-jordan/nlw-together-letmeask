18ebb8da1d1dfba1c21a8a683fdf4fc21e2f4431
Brayan <brayan.11fera@gmail.com>
2021-09-01 22:27:27 -0300

    Problema de versao do firebase arrumado

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,4 +1,4 @@
-import * as firebase from "firebase/app";
+import  firebase from "firebase/app";
 
 // firebase recomenda que faca a importacao de cada servico que for utilizar,
 // como vamos utilizar autenticacao com google e banco de dados esses serao os importados
