6076b9014ace1c16e8f88065d8484d111386e7dc
Brayan Vinicius Jordan <80286232+brayanJordan@users.noreply.github.com>
2021-09-29 17:51:49 -0300

    Criacao da pagina de admin de sala

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,21 @@ import { ButtonHTMLAttributes } from "react"
 
 import '../styles/button.scss';
 
+// esse & serve para alem de receber os atributos padroes de um botao html ele tambem pode receber outras variaveis
+// conforme sera mostrado a seguir
 // uma tipagem do proprio react que permite passar qualquer tipagem do padrao html para os botoes atraves do props 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement >
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+}
 
-export function Button(props: ButtonProps) {
+// pegando o is outlined, e o resto esta jogando dentro de um props atraves do ... (rest operator)
+export function Button({ isOutlined = false, ...props}: ButtonProps) {
   return (
     // esse ...props, toda vez que chamar esse componente passara todos os type, submit, etc diretamente pra esse botao
-    <button className="button" {...props}/>
+    <button 
+      // classname button e caso o isOutlined for true vai adicionar mais um classname no caso outlined
+      className={`button ${isOutlined ? 'outlined': ''}`}
+      {...props}
+    />
   )
 }
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -14,52 +14,23 @@ type RoomParams = {
   id: string
 }
 
-
-export function Room() {
+export function AdminRoom() {
   const { user } = useAuth()
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('')
   const roomId = params.id
   const { questions, title } = useRoom(roomId)
 
-  
-  // o useffect e uma funcao que espera a atualizacao de algo para ser executado, dentro do array no final a gente
-  // passa o que ele fica esperando atualizacao (caso vazio ele vai executar apenas uma vez durante o carregamento da pagina)
-
-  // toda vez que uma funcao for colocada em um formulario com onsubmit precisa tipar com o event: formevent
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault()
-
-    if(newQuestion.trim() === '') {
-      return;
-    }
-
-    if (!user) {
-      throw new Error('Voce nao esta autenticado');
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar
-      },
-      isHighlighted: false,
-      isAnswered: false
-    }
-
-    await database.ref(`rooms/${roomId}/questions`).push(question)
-
-    setNewQuestion('')
-
-  }
-
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode code={roomId} />
+          <div>
+            <RoomCode code={roomId} />
+            {/*  por ser um booleano o isOutlined nao precisa por = true */}
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
         </div>
       </header>
 
@@ -71,25 +42,6 @@ export function Room() {
           {questions.length > 0 && <span>{questions.length} Perguntas</span>}
           
         </div>
-
-        <form onSubmit={handleSendQuestion}>
-          <textarea 
-            placeholder="O que voce quer perguntar?"
-            onChange={event => setNewQuestion(event.target.value)}
-            value={newQuestion}
-          />
-          <div className="form-fotter">
-            {user ? (
-              <div className="user-info"> 
-                <img src={user.avatar} alt={user.name} />
-                <span>{user.name}</span>
-              </div>
-            ) : (
-            <span>Para enviar uma pergunta, <button> faca seu login </button>.</span>
-            ) }
-            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
-          </div>
-        </form>
 
         <div className="question-list">
           {
